c42b8af4d3e70572403484fad2a17c5ad09a384a
张小华 <zhangronghua309@sina.com>
2014-10-10 19:05:29 +0800

    添加表达式计算类
    添加表达式计算类

--- src/com/killy/calculate/StartGameFace.java
+++ src/com/killy/calculate/StartGameFace.java
@@ -14,6 +14,9 @@ import android.view.View;
 import android.view.View.OnTouchListener;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.killy.calculate.Eval.ExpressionException;
 
 public class StartGameFace extends Activity {
 
@@ -40,7 +43,7 @@ public class StartGameFace extends Activity {
 	/**
 	 * 数字按钮部分与运算符按钮部分的间隔长度
 	 */
-	private static final int SplitHeight = 10;
+	private static final int splitHeight = 10;
 
 	/**
 	 * 存放整个页面的所有数字按钮，采用二维数组 第一维是按钮的横坐标 第二维是按钮的纵坐标
@@ -51,12 +54,19 @@ public class StartGameFace extends Activity {
 	 * 存放整个页面的所有运算符按钮，采用二维数组 第一维是按钮的横坐标 第二维是按钮的纵坐标
 	 */
 	private static final Button calBtnArr[][] = new Button[calHeiNum][widNum];
+	
+	/**
+	 * 存放整个运算表达式的按钮
+	 */
+	private static final Button expressBtnArr[] = new Button[widNum];
 
+	private static int leftBtn = widNum;
+	
 	/**
 	 * 运算符列表
 	 */
-	private static final String[] calArr = { "+", "-", "*", "/" };
-
+	private static final String[] calArr = { "+", "-", "*", "/", "="};
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -76,7 +86,7 @@ public class StartGameFace extends Activity {
 				// 按钮的ID是通过2000+10*横序号+纵序号(考虑到一横最多不会超过10个按钮,一纵也不会超过10个)
 				numBtnArr[i][j].setId(getNumId(j, i));
 				numBtnArr[i][j].setText(""
-						+ Math.abs(random.nextInt(i * SplitHeight + j + 1)));
+						+ Math.abs(random.nextInt(i * splitHeight + j + 1)));
 				addMoveLister(numBtnArr[i][j]);
 
 				GradientDrawable drawable = new GradientDrawable();
@@ -112,7 +122,7 @@ public class StartGameFace extends Activity {
 				RelativeLayout.LayoutParams btParams = new RelativeLayout.LayoutParams(
 						btnWidth, btnHeight); // 设置按钮的宽度和高度
 				// i表示的是第i行 k表示的是第k列
-				btParams.topMargin = btnHeight * (i + numHeiNum) + SplitHeight; // 纵坐标定位
+				btParams.topMargin = btnHeight * (i + numHeiNum) + splitHeight; // 纵坐标定位
 				btParams.leftMargin = btnWidth * j; // 横坐标定位
 				relativeLayout.addView(calBtnArr[i][j], btParams);
 			}
@@ -120,19 +130,39 @@ public class StartGameFace extends Activity {
 
 		// 添加待填充的运算格子
 		RelativeLayout waitCalLayout = new RelativeLayout(this);
-		waitCalLayout.layout(0, btnHeight * (numHeiNum + calHeiNum + SplitHeight * 2), screenWidth, screenHeight);
+		waitCalLayout.layout(0, btnHeight * (numHeiNum + calHeiNum + splitHeight * 2), screenWidth, screenHeight);
 		GradientDrawable drawable = new GradientDrawable();
 		drawable.setShape(GradientDrawable.RECTANGLE); // 画框
 		drawable.setStroke(2, Color.RED); // 边框粗细及颜色
 		drawable.setColor(0xFF662200); // 边框内部颜色
 		waitCalLayout.setBackground(drawable);
 		RelativeLayout.LayoutParams layParams = new RelativeLayout.LayoutParams(
-				screenWidth - 10, screenHeight - 10 - btnHeight * (numHeiNum + calHeiNum + SplitHeight * 2)); // 设置按钮的宽度和高度
+				screenWidth - 10, screenHeight - 10 - btnHeight * (numHeiNum + calHeiNum + splitHeight * 2)); // 设置按钮的宽度和高度
 		// i表示的是第i行 k表示的是第k列
-		layParams.topMargin = btnHeight * (numHeiNum + calHeiNum + SplitHeight * 2); // 纵坐标定位
+		layParams.topMargin = btnHeight * (numHeiNum + calHeiNum + splitHeight * 2); // 纵坐标定位
 		layParams.leftMargin = 0; // 横坐标定位
 		relativeLayout.addView(waitCalLayout, layParams);
 
+		// 添加结果显示
+		TextView totalScore = new TextView(this);
+		totalScore.setText("总得分：0");
+		totalScore.setTextColor(Color.BLACK);
+		RelativeLayout.LayoutParams totalScoreParams = new RelativeLayout.LayoutParams(
+				50, 20); // 设置按钮的宽度和高度
+		// i表示的是第i行 k表示的是第k列
+		totalScoreParams.topMargin = btnHeight * (numHeiNum + calHeiNum + splitHeight * 2) + btnHeight + 10; // 纵坐标定位
+		totalScoreParams.leftMargin = 0; // 横坐标定位
+		relativeLayout.addView(totalScore, totalScoreParams);
+		
+		TextView totalCnt = new TextView(this);
+		totalCnt.setText("总成功次数：0");
+		RelativeLayout.LayoutParams totalCntParams = new RelativeLayout.LayoutParams(
+				50, 20); // 设置按钮的宽度和高度
+		// i表示的是第i行 k表示的是第k列
+		totalCntParams.topMargin = btnHeight * (numHeiNum + calHeiNum + splitHeight * 2) + btnHeight + 10; // 纵坐标定位
+		totalCntParams.leftMargin = 50 + 10; // 横坐标定位
+		relativeLayout.addView(totalCnt, totalCntParams);
+		
 		setContentView(relativeLayout);
 	}
 
@@ -231,9 +261,44 @@ public class StartGameFace extends Activity {
 		
 		int btnSize = screenWidth / widNum;
 		// 当该按钮的超过一半的部分已经落入运算表达式框内，则标识该按钮被拉入运算表达式框内
-		if (t > btnSize * (numHeiNum + calHeiNum) - btnSize / 2 + 2 * SplitHeight) {
-			l = btnSize * (l / btnSize);
-//			t = 
+		if (t > btnSize * (numHeiNum + calHeiNum) - btnSize / 2 + 2 * splitHeight) {
+			if (l % btnSize * 2 > btnSize) {
+				l = btnSize * (l / btnSize) + btnSize;
+			} else {
+				l = btnSize * (l / btnSize);
+			}
+			t = btnSize * (numHeiNum + calHeiNum) + 2 * splitHeight;
+			while (null != expressBtnArr[l / btnSize]) {
+				l = l + btnSize;
+				if (l / btnSize > widNum) {
+					l = 0;
+				}
+			}
+			v.layout(l, t, l + btnSize, t + btnSize);
+			expressBtnArr[l / btnSize] = (Button)v;
+			leftBtn--;
+			if (leftBtn == 0) {
+				// 倒数第二个运算符必须是=号
+				if (!"=".equals(expressBtnArr[widNum - 2].getText())) {
+					
+				} else {
+					StringBuilder express = new StringBuilder();
+					for (int i = 0; i < widNum - 2; i++) {
+						express.append(expressBtnArr[i].getText());
+					}
+					Eval eval = new Eval();
+					Object result;
+					try {
+						result = eval.calculate(express.toString());
+						// 计算的结果不相等
+						if (Integer.parseInt(expressBtnArr[widNum - 1].getText().toString()) != Integer.parseInt(result.toString())) {
+							
+						}
+					} catch (ExpressionException e) {
+						e.printStackTrace();
+					}
+				}
+			}
 		} else {
 			if (isNumBtn(v.getId())) {
 				int top = btnSize * (v.getId() / 10);
@@ -242,7 +307,7 @@ public class StartGameFace extends Activity {
 			} else if (isCalBtn(v.getId())) {
 				int top = btnSize * (v.getId() / 10);
 				int left = btnSize * (v.getId() % 10);
-				v.layout(left, top + 10, left + btnSize, top + btnSize + SplitHeight);
+				v.layout(left, top + 10, left + btnSize, top + btnSize + splitHeight);
 			}
 		}
 	}
